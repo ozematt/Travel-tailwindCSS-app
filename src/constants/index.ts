54e629d1a0fdbcdc73dac6d41217cb9636d5ab59
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-20 09:58:43 +0100

    added constants with nav link

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const navLinks = [
+  { href: "#home", label: "Home" },
+  { href: "#about-us", label: "About Us" },
   { href: "#destination", label: "Destination" },
-  { href: "#hotels", label: "Hotels" },
-  { href: "#flights", label: "Flights" },
   { href: "#bookings", label: "Contact Us" },
 ];
