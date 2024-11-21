e65c1e773f175227615665d7db082c5434649e0e
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-21 13:11:50 +0100

    added media queries

--- src/sections/Companies.tsx
+++ src/sections/Companies.tsx
@@ -3,13 +3,13 @@ import { companies } from "../constants";
 const Companies = () => {
   return (
     <section className="px-[34px] sm:px-[120px] mt-[100px] sm:mt-[200px] w-full relative">
-      <div className="flex items-center justify-evenly">
+      <div className="flex flex-wrap items-center justify-evenly">
         {companies.map((company) => (
           <img
             key={company.name}
             src={company.imgURL}
             alt={company.name}
-            className="grayscale w-[200px] h-[100px] rounded-2xl object-scale-down hover:shadow-customMorph hover:grayscale-0 hover:scale-110 transition duration-300"
+            className=" hidden md:block grayscale w-[200px] h-[100px] rounded-2xl object-scale-down hover:shadow-customMorph hover:grayscale-0 hover:scale-110 transition duration-300"
           />
         ))}
       </div>
