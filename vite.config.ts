76cf981928d4421b0b80cfffe42eb08cf86be18d
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-23 18:28:02 +0100

    installed and configured vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+  test: {
+    coverage: {
+      reporter: ["cobertura", "json", "html"],
+    },
+    environment: "jsdom",
+    globals: true,
+    include: ["src/**/*.{spec,test}.{ts,tsx}"],
+    setupFiles: ["/setupTests.mjs"],
+  },
+});
