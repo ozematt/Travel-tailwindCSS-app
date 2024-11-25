b89a3a531491e60cd4d2e52744dbb688016ce308
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-25 20:45:15 +0100

    added sign in form logic

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -3,11 +3,19 @@ import decoration from "../assets/Decore3.png";
 import { useEffect, useId, useState } from "react";
 import { z } from "zod";
 
-const UserSchema = z.object({
-  id: z.string(),
-  user: z.string().email(),
-  password: z.string().min(6),
-});
+const UserSchema = z
+  .object({
+    id: z.string(),
+    userEmail: z.string().email({ message: "Invalid email" }),
+    userPassword: z
+      .string()
+      .min(6, { message: "Must be at last 6 characters long " }),
+    userPasswordRepeat: z.string().min(6).optional(),
+  })
+  .refine((data) => data.userPassword === data.userPasswordRepeat, {
+    message: "Password must match",
+    path: ["passwordRepeat"],
+  });
 
 const UsersArraySchema = z.array(UserSchema);
 
@@ -20,11 +28,15 @@ const SignUp = () => {
   ////DATA
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
-  const [error, setError] = useState<string | null>(null);
+  const [userPasswordRepeat, setUserPasswordRepeat] = useState("");
+  const [errors, setErrors] = useState<Record<string, string>>({});
   const [users, setUsers] = useState<UsersArray>([]);
+
+  console.log(errors);
 
   const id = useId();
 
+  //fetch data from local storage
   useEffect(() => {
     const rawUsers = JSON.parse(localStorage.getItem("users") || "[]");
 
@@ -40,16 +52,44 @@ const SignUp = () => {
   //Submit sign in form
   const handleSignUp = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    //form data validation
+    const result = UserSchema.safeParse({
+      id,
+      userEmail,
+      userPassword,
+      userPasswordRepeat,
+    });
+    if (!result.success) {
+      console.log(result.error.issues);
+      //assign errors to object
+      const formattedErrors = result.error.errors.reduce((acc, error) => {
+        if (error.path[0]) {
+          acc[error.path[0]] = error.message;
+        }
+        return acc;
+      }, {} as Record<string, string>);
+
+      setErrors(formattedErrors);
+      return;
+    } else {
+      setErrors({});
+    }
+
     //checking if user already exist
-    const checkUserId = users.some((user) => user.user === userEmail);
+    const checkUserId = users.some((user) => user.userEmail === userEmail);
     if (checkUserId) {
       //set error and stop if exist
-      setError("User already exist");
+      setErrors({ userEmail: "User already exist" });
       return;
     }
 
     // make uniq user
-    const newUser: User = { id: id, user: userEmail, password: userPassword };
+    const newUser: User = {
+      id: id,
+      userEmail: userEmail,
+      userPassword: userPassword,
+    };
     //updated state with all users
     const updatedUsers = [...users, newUser];
     setUsers(updatedUsers);
@@ -60,6 +100,7 @@ const SignUp = () => {
     //clear form
     setUserEmail("");
     setUserPassword("");
+    setUserPasswordRepeat("");
   };
 
   return (
@@ -95,6 +136,13 @@ const SignUp = () => {
             placeholder="Your password"
             value={userPassword}
             onChange={(e) => setUserPassword(e.target.value)}
+            className="bg-no-repeat bg-[center_left_1.5rem] focus:outline-none focus:ring-1 ring-black pl-[3.2rem]  w-full max-w-[500px] h-[70px] rounded-xl placeholder:pl-1"
+          />
+          <input
+            type="passwordRepeat"
+            placeholder="Repeat password"
+            value={userPasswordRepeat}
+            onChange={(e) => setUserPasswordRepeat(e.target.value)}
             className="bg-no-repeat bg-[center_left_1.5rem] focus:outline-none focus:ring-1 ring-black pl-[3.2rem]  w-full max-w-[500px] h-[70px] rounded-xl placeholder:pl-1"
           />
 
