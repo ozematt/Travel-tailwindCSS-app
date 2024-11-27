8bd5e7ae2c35439a88d1253be53d83000ba54510
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-27 15:39:29 +0100

    test Ci

--- src/tests/Button.spec.tsx
+++ src/tests/Button.spec.tsx
@@ -7,24 +7,24 @@ import userEvent from "@testing-library/user-event";
 describe("<Button/>", () => {
   it("should render button with correct label", () => {
     render(<Button>Login</Button>);
-
     const button = screen.getByRole("button", { name: "Login" });
+
     expect(button).toBeInTheDocument();
   });
   it("should respond to click events", async () => {
     const user = userEvent.setup();
     const onClick = vi.fn();
+
     render(<Button onClick={onClick}>Login</Button>);
-
     const button = screen.getByRole("button", { name: "Login" });
-
     await user.click(button);
+
     expect(onClick).toBeCalledTimes(1);
   });
   it("should render button with correct label", () => {
     render(<Button type="submit">Login</Button>);
-
     const button = screen.getByRole("button", { name: "Login" });
+
     expect(button).toHaveAttribute("type", "submit");
   });
 });
